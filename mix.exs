4bbb3c0e2ace022fbf26bd35a3abd266d5fcc38a
Anthony Falzetti <afalzetti@comverge.com>
2019-04-04 11:56:12 -0600

    Update poison to ~> 3.0
    Allow :pid to be passed in as metadata in string format

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule LoggerJSONFileBackend.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 1.5 or ~> 2.0"},
+      {:poison, "~> 3.1"},
       {:uuid, "~> 1.1"},
       {:json, "~> 0.3.2", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev},
